bc20f600088c6dc4b2a61c0edf762e5d30eb72cf
isoUZH <108625265+isoUZH@users.noreply.github.com>
2022-07-03 18:47:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple package for Curve Sketching",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/isoUZH/curvesketching.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
